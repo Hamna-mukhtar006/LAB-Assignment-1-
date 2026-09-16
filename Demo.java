33f946a59e25be6a538508aacca7f279116ebdb9
HAMNA MUKHTAR <sp26-bcs-021@cuilahore.edu.pk>
2026-09-16 17:25:31 +0500

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -1,12 +1,20 @@
 public class Demo{
-	public static void main(String[] args){
-		Student A = new Student();
-			A.id = 78;
-			A.name = "Hamna";
-			A.completedCredits = 30;
+	public static void main (String[] args){
+		Student s1 = new Student();
+			s1.id = 23;
+			s1.name = "Hamna";
+			s1.completedCredits = 30;
 			
-			A.printSummary();
+			
+                 
+		Student s2 = new Student();
+			s2.id = 24;
+			s2.name = "Iqra";
+			s2.completedCredits = 30;
 
-}
-		}
-	
+			
+
+	s1.printSummary();
+	s2.printSummary();
+	s1.addcredit (20);
+}} 
--- Student.java
+++ Student.java
@@ -6,5 +6,8 @@ public class Student{
 	void printSummary(){
 		System.out.println("Id : "+id);
 		System.out.println("Name : "+name);
-		System.out.println("Credits completed"+completedCredits);}
+		System.out.println("Credits completed : "+completedCredits);}
+	void addcredit(int amount){
+		completedCredits = completedCredits + amount;
+		System.out.println("Credits completed : "+completedCredits);}
 }
